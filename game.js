514270584d84af8ccf9abc69d4f82bf402290e7a
Joshua Konstantinos <77114591+Joshfkon@users.noreply.github.com>
2024-02-23 15:58:04 -0500

    revert display digits

--- game.js
+++ game.js
@@ -71,8 +71,8 @@ let tasks = {
 
 
 function updateDisplay() {
-    document.getElementById('perishable-food-count').textContent = parseFloat(perishableFood).toFixed(2);
-    document.getElementById('preserved-food-count').textContent = parseFloat(preservedFood).toFixed(2);
+    document.getElementById('perishable-food-count').textContent = perishableFood;
+    document.getElementById('preserved-food-count').textContent = preservedFood;
     document.getElementById('population-count').textContent = population;
 }
 
